843772938b2ce3bb67ddedb08ac3e77d073a1773
luasilveira <108275861+luasilveira@users.noreply.github.com>
2022-06-27 09:43:55 -0300

    Add files via upload

---
+++ exer1.java
@@ -0,0 +1,10 @@
+public class exer1 {
+    public static void main(String[] args) {
+        int h = 3;
+        int b = 5;
+        int B = 7;
+        int Area;
+        Area = (h * (b + B)) / 2;
+        System.out.println("O resultado da área é:" + Area);
+    }
+}
