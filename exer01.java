86f3b212fd70aebb6b4904386eb58182055392e2
luasilveira <108275861+luasilveira@users.noreply.github.com>
2022-07-07 20:42:20 -0300

    Create exer01.java

---
+++ exer01.java
@@ -0,0 +1,14 @@
+public class exer01 {
+   /** 
+    * @param args
+    * @return 
+    */
+   public static void main(final String[] args) {
+      // Área do trapézio
+      int h = 5;
+      int b = 3;
+      int B = 7;
+      double Area = (h * (b + B))/2;
+      System.out.println("A área do trapézio é " + Area);}
+   
+}
