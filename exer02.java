eba46166f8fa84550e1285e398db5c8d611057b2
luasilveira <108275861+luasilveira@users.noreply.github.com>
2022-07-07 21:05:26 -0300

    Create exer02.java

---
+++ exer02.java
@@ -0,0 +1,16 @@
+public class exer02 {
+    /** 
+     * @param args
+     * @return 
+     */
+    public static void main(final String[] args) {
+       // Área do trapézio
+       double h = 7.2;
+       int b = 3;
+       double B = 8.6;
+       double Area = (h * (b + B))/2;
+       System.out.println("O valor exato da área é " + Area);
+       System.out.println("O valor arredondado da área é " + (Math.round(42)));}
+    
+}
+ 
